b00da2b9b4caae2cd620f9d99eba156fcef854de
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-07-29 16:44:43 +0300

    base impl

---
+++ algo/Climbing Stairs /base.cpp
@@ -0,0 +1,24 @@
+
+#include "base.h"
+#include "iostream"
+
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
+
+
+
+int main() {
+    Solution solution;
+
+    std::cout << solution.climbStairs(5);
+}
---
+++ algo/Climbing Stairs /base.h
@@ -0,0 +1,12 @@
+
+
+#ifndef ABSTRACT_REPO_BASE_H
+#define ABSTRACT_REPO_BASE_H
+
+
+class base {
+
+};
+
+
+#endif //ABSTRACT_REPO_BASE_H
