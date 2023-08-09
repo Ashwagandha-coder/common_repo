4f02785bd4ddc4cd9559a553c2fc8af710e9b976
Danil M <58209188+ashwagandha-coder@users.noreply.github.com>
2023-08-09 16:29:15 +0300

    add atomic

---
+++ common/AtomicReference.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Мухамадиев Данил Дамирович on 09.08.2023.
+//
+
+#include "AtomicReference.h"
---
+++ common/AtomicReference.h
@@ -0,0 +1,14 @@
+//
+// Created by Мухамадиев Данил Дамирович on 09.08.2023.
+//
+
+#ifndef ABSTRACT_REPO_ATOMICREFERENCE_H
+#define ABSTRACT_REPO_ATOMICREFERENCE_H
+
+
+class AtomicReference {
+
+};
+
+
+#endif //ABSTRACT_REPO_ATOMICREFERENCE_H
